70bf5b9b53b8bfd79b31e434db482fe418f4cb6e
DiegoAlejandroRJ <diego6382@gmail.com>
2024-02-20 16:27:11 -0500

    memoria h2

---
+++ src/main/java/com/darjh2/H2memory/H2memoryApplication.java
@@ -0,0 +1,13 @@
+package com.darjh2.H2memory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2memoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2memoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darjh2/H2memory/H2memoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darjh2.H2memory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2memoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
